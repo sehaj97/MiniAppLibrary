1dd42e4ff45c3cf5c2063fa382f8512e9b9d98f0
Sehajpreet Singh <sehajmagan@gmail.com>
2024-03-05 20:42:15 -0500

    converted the hook to general api fetch hook

--- src/Pages/FlagsApp/index.jsx
+++ src/Pages/FlagsApp/index.jsx
@@ -1,7 +1,7 @@
-import useFlags from "../../Utilities/CustomHooks/useFlags";
+import useApiFetch from "../../Utilities/CustomHooks/useApiFetch";
 
 const FlagsApp = () => {
-    const { flagData, isLoading, isError } = useFlags();
+    const { flagData, isLoading, isError } = useApiFetch('https://countriesnow.space/api/v0.1/countries/flag/images');
     if (isError) return
     <div className={`container mt-32 md:w-3/6 lg:1/6 mx-auto flex flex-col justify-content-center align-middle gap-y-3 bg-lightPink p-8 rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-300 ease-in-out`}>
         <h1 className="text-2xl font-semibold mb-4 text-center">Error Occurred</h1>
